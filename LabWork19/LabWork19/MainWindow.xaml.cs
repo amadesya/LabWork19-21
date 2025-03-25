34668d19e270ac4c9d4836038bde8989b5f07054
amadesya <154593967+amadesya@users.noreply.github.com>
2025-03-25 22:18:47 +0300

    Update MainWindow.xaml.cs

--- LabWork19/LabWork19/MainWindow.xaml.cs
+++ LabWork19/LabWork19/MainWindow.xaml.cs
@@ -1,15 +1,11 @@
-﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using MySql.Data.MySqlClient;
 
 namespace LabWork19
 {
     public partial class MainWindow : Window
     {
-        private string Server = "localhost";
-        private string Database = "market"; 
-        private string Port = "3306"; 
+        private DAL dal = new DAL(); 
 
         public MainWindow()
         {
@@ -28,7 +24,7 @@ namespace LabWork19
                 return;
             }
 
-            if (CheckDatabaseConnection(username, password))
+            if (dal.CheckDatabaseConnection(username, password)) 
             {
                 MessageBox.Show($"Здравствуйте, {username}!", "Успешный вход", MessageBoxButton.OK, MessageBoxImage.Information);
 
@@ -52,23 +48,6 @@ namespace LabWork19
             {
                 lblMessage.Text = "Ошибка входа! Неверный логин или пароль.";
                 lblMessage.Visibility = Visibility.Visible;
-            }
-        }
-
-        private bool CheckDatabaseConnection(string username, string password)
-        {
-            try
-            {
-                string connectionString = $"Server={Server};Database={Database};User ID={username};Password={password};Port={Port};";
-                using (MySqlConnection conn = new MySqlConnection(connectionString))
-                {
-                    conn.Open();
-                    return true; // Успешное подключение
-                }
-            }
-            catch
-            {
-                return false; // Ошибка подключения
             }
         }
     }
